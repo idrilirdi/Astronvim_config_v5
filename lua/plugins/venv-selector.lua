f8dfa732cacb2a0f41ca2f7073f2c193e73df711
Idril Geer <idril.geer@futurail.com>
2026-01-09 12:12:54 +0100

    update venv-selector

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,17 +1,18 @@
 return {
-  'linux-cultist/venv-selector.nvim',
+  "linux-cultist/venv-selector.nvim",
   name = "venv-selector",
   dependencies = {
     "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
+    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python", --optional
     { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
   lazy = false,
-  branch = "regexp",  -- This is the regexp branch, use this for the new version
-  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  --  branch = "regexp",  -- This is the regexp branch, use this for the new version
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
-    { '<leader>vs', '<cmd>VenvSelect<cr>', desc = 'venv selector' },
+    { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "venv selector" },
   },
   ---@type venv-selector.Config
   opts = {
